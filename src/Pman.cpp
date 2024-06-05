18511ebb89576524a2b74cc5e935789edfab4f25
Biel Sala <biel.sala@autonoma.cat>
2024-06-05 16:34:48 +0200

    List subcmd (doesnt even work)

--- src/Pman.cpp
+++ src/Pman.cpp
@@ -1,24 +1,42 @@
 #include "Pman.hpp"
 
 #include <iostream>
+#include <cstdlib>
+#include <filesystem>
+
+const std::string program_name = "pman";
 
 Pman::Pman(int argc, char** argv)
 {
     m_cmds.push_back(Cmd { .id = "help", .s = "h", .l = "help", .usage = "Print help message", .params = "" });
     m_cmds.push_back(Cmd { .id = "list", .s = "l", .l = "list", .usage = "List passwords", .params = "" });
-    
+
+    char* HOME = std::getenv("HOME");
+
+    m_HOME = HOME != NULL ? std::filesystem::path(HOME) : std::filesystem::path("");
+
+    m_config_path = m_HOME;
+    m_config_path /= "config";
+    m_config_path /= program_name;
+    std::filesystem::create_directory(m_config_path);
+
+    m_passwords_path = m_config_path;
+    m_passwords_path /= "passwords";
+    std::filesystem::create_directory(m_passwords_path);
+
     m_argc = argc;
     m_argv = argv;
-    m_has_subcommand = false;
+    m_has_subcmd = false;
     m_has_args = false;
+
     if (m_argc == 1) {
-        m_has_subcommand = false;
+        m_has_subcmd = false;
     }
 
     if (m_argc > 1) {
-        m_has_subcommand = true;
+        m_has_subcmd = true;
     }
-    
+
     if (m_argc > 2) {
         m_has_args = true;
     }
@@ -26,15 +44,15 @@ Pman::Pman(int argc, char** argv)
 
 void Pman::parse()
 {
-    m_subcommand = "";
-    if (Pman::has_subcommand()) {
+    m_subcmd_id = "";
+    if (Pman::has_subcmd()) {
         for (Cmd cmd : m_cmds) {
             if (m_argv[1] == cmd.s || m_argv[1] == cmd.l) {
-                m_subcommand = cmd.id;
+                m_subcmd_id = cmd.id;
             }
         }
-        if (m_subcommand == "") {
-            m_has_subcommand = false;
+        if (m_subcmd_id == "") {
+            m_has_subcmd = false;
         }
     }
 
@@ -45,10 +63,10 @@ void Pman::parse()
     }
 }
 
-void Pman::print_subcommand() const
+void Pman::print_subcmd() const
 {
-    if (Pman::has_subcommand()) {
-        std::cerr << "Subcommand: " << m_subcommand << std::endl;
+    if (Pman::has_subcmd()) {
+        std::cerr << "Subcommand: " << m_subcmd_id << std::endl;
     }
 }
 
@@ -65,7 +83,7 @@ void Pman::print_args() const
 
 bool Pman::subcommand_is(std::string id) const
 {
-    return m_subcommand == id;
+    return m_subcmd_id == id;
 }
 
 int Pman::subcmd_help()
@@ -80,8 +98,12 @@ int Pman::subcmd_help()
 
 int Pman::subcmd_list()
 {
-    // TODO: List ~/.config/pman/passwords
-    // Might need to get env variables on the constructor
+    std::cout << "Listing directory " << m_passwords_path.generic_string() << ": " << std::endl;
+    std::filesystem::directory_iterator dir(m_passwords_path);
+    for (auto const& entry : dir) {
+        std::cout << entry.path() << std::endl;
+    }
+    
     return 0;
 }
 
@@ -89,7 +111,7 @@ int Pman::exec()
 {
     Pman::parse();
 
-    if (!Pman::has_subcommand()) {
+    if (!Pman::has_subcmd()) {
         std::cerr << "Error: No valid subcommand provided" << std::endl;
         return Pman::subcmd_help();
     }
@@ -100,6 +122,6 @@ int Pman::exec()
     } else if (Pman::subcommand_is("list")) {
         return Pman::subcmd_list();
     }
-    
+
     return 0;
 }
--- src/Pman.hpp
+++ src/Pman.hpp
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <string>
+#include <filesystem>
 
 #include "Cmd.hpp"
 
@@ -13,19 +14,26 @@ public:
     int exec();
 
 private:
-    bool has_subcommand() const { return m_has_subcommand; }
+    bool has_subcmd() const { return m_has_subcmd; }
     bool has_args() const { return m_has_args; }
     bool subcommand_is(std::string id) const;
-    void print_subcommand() const;
+    void print_subcmd() const;
     void print_args() const;
     void parse();
     int subcmd_help();
     int subcmd_list();
+
     int m_argc;
     char** m_argv;
-    bool m_has_subcommand;
+
+    std::filesystem::path m_HOME;
+    std::filesystem::path m_config_path;
+    std::filesystem::path m_passwords_path;
+
+    bool m_has_subcmd;
     bool m_has_args;
-    std::string m_subcommand;
+
+    std::string m_subcmd_id;
     std::vector<std::string> m_args;
     std::vector<Cmd> m_cmds;
 };
